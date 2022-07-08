9671be2d4656e995c630f74106c2adb0e372642b
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-07-08 22:11:53 +0530

    incomeexpense

---
+++ authentication/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from .views import VerifyEmail, RegisterView, LoginAPIView
+
+
+urlpatterns =[
+    path('register/', RegisterView.as_view(), name ="register"),
+    path('login/', LoginAPIView.as_view(), name="login"),
+    path('email-verify/', VerifyEmail.as_view(), name= "email-verify")
+
+]
